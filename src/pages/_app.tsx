469337885a1b394f4140bee45eddfb5e3f194a79
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2023-04-18 14:01:42 +0900

    using useEffect() and router to set the initial page to the create-video

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from './components/Layout'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  //setting the initial page as create-video
+  useEffect(() => {
+   router.push('/create-video')
+
+  }, [])
+
+
   return (
   <Layout>
     <Component {...pageProps} />
